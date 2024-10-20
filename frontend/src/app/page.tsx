601515751b8c521ebe14728359efe51820c77b73
Felipe Simões <ffelipesimoes@gmail.com>
2024-10-20 02:46:58 -0700

    update page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,6 +4,13 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl sm:text-5xl font-bold text-center sm:text-left">
+          Buy crypto with ease{" "}
+          <span className="text-4xl sm:text-5xl pt-4 ">with</span> <br />{" "}
+          <span className="text-5xl sm:text-6xl  text-blue-500">
+            SwiftStable
+          </span>
+        </h1>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
@@ -18,7 +25,22 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Buy now
+            On Ramp
+          </a>
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://offramp-sandbox.gatefi.com/?merchantId=a8c450fb-6914-4308-8dbc-b4b5b9cbbead"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="https://nextjs.org/icons/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Off Ramp
           </a>
         </div>
       </main>
